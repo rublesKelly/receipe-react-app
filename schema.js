8ef5416cae9cbc49774eae6ae231b8d65635802b
rublesKelly <rkellymkk@gmail.com>
2021-06-29 13:20:19 +0100

    go away bitconnec !!

--- schema.js
+++ schema.js
@@ -1,6 +1,12 @@
 //This is where all the graphql stuff goes this is where we define our schema with objects and such 
+const   {GraphQLObjectType, GraphQLInt, 
+        GraphQLString, GraphQLBoolean} = require('graphql');
 
-
-const {GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLBoolean} = require('graphql');
-
-// 
+//Create reciepe type
+const receipeType = new GraphQLObjectType({
+    name: 'uinset name here',
+    description: 'stuff goes here bro',
+    fields () => ({
+        field1: {type: GraphQLString}
+    })
+})
